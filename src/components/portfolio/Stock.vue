0f0ba79583d7a4a18654034f6841191fcfa9dd06
douglas <douglas06mp@gmail.com>
2020-06-17 17:02:53 +0800

    fix sell method in Stock component for portfolio and bind key in v-for

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,33 +1,42 @@
 <template>
-    <div class="col-lg-4 col-md-6">
-        <div class="card border-info mb-3">
-            <div class="card-header bg-info text-white">{{ stock.name }} <span style="font-size: .2rem;">(Price: {{ stock.price }} | Quantity: {{ stock.quantity }})</span></div>
-            <div class="card-body text-info" style="display: flex; justify-content: space-between;">
-                <input type="number" v-model.number="quantity">
-                <button class="btn btn-info" @click="sell" :disabled="quantity < 0 || quantity > stock.quantity">Sell</button>
-            </div>
-        </div>
+  <div class="col-lg-4 col-md-6">
+    <div class="card border-info mb-3">
+      <div class="card-header bg-info text-white">
+        {{ stock.name }}
+        <span
+          style="font-size: .2rem;"
+        >(Price: {{ stock.price }} | Quantity: {{ stock.quantity }})</span>
+      </div>
+      <div class="card-body text-info" style="display: flex; justify-content: space-between;">
+        <input type="number" v-model.number="quantity" />
+        <button
+          class="btn btn-info"
+          @click="sell"
+          :disabled="quantity < 0 || quantity > stock.quantity"
+        >Sell</button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: ['stock'],
-        data() {
-            return {
-                quantity: 0
-            }
-        },
-        methods: {
-            sell(){
-                const order = {
-                    id: this.stock.id,
-                    price: this.stock.quantity,
-                    quantity: this.quantity
-                }
-                this.$store.dispatch('sellStock', order)
-                this.quantity = 0
-            }
-        }
+export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  methods: {
+    sell() {
+      const order = {
+        id: this.stock.id,
+        price: this.stock.price,
+        quantity: this.quantity
+      };
+      this.$store.dispatch("sellStock", order);
+      this.quantity = 0;
     }
+  }
+};
 </script>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="col col-xs-12" style="display: flex; flex-wrap: wrap;">
-        <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
-    </div>
+  <div class="col col-xs-12" style="display: flex; flex-wrap: wrap;">
+    <app-stock v-for="stock in stocks" :stock="stock" :key="stock.id"></app-stock>
+  </div>
 </template>
 
 <script>
-    import Stock from './Stock'
-    export default {
-        computed: {
-            stocks(){
-                return this.$store.getters.stocks
-            }
-        },
-        components: {
-            appStock: Stock
-        }
+import Stock from "./Stock";
+export default {
+  computed: {
+    stocks() {
+      return this.$store.getters.stocks;
     }
+  },
+  components: {
+    appStock: Stock
+  }
+};
 </script>
