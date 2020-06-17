b3e0b4192b78680080369ef89e74ebe76830729d
douglas <douglas06mp@gmail.com>
2020-06-17 16:49:49 +0800

    rename github repo and npm audit fix

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/stockTrader/' : '/'
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-stock-trader/" : "/"
+};
